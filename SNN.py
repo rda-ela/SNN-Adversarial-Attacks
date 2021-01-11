441dfa40d42eb0d068ac24543a57706c8836c9ae
rda-ela <75096935+rda-ela@users.noreply.github.com>
2021-01-11 15:15:09 +0100

    Cleaned SNN.py

--- SNN.py
+++ SNN.py
@@ -1,12 +1,9 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 import torch
 import torchvision
 
 import torchvision.transforms as transforms
-
-from typing import NamedTuple
 
 from norse.torch.functional.lif import LIFParameters
 from norse.torch.module.leaky_integrator import LICell
@@ -15,11 +12,34 @@ from norse.torch.module.lif import LIFFeedForwardCell
 import norse.torch.functional.encode as encode
 
 import foolbox as fb
-from foolbox import PyTorchModel, accuracy, samples
 import foolbox.attacks as fa
+from foolbox import PyTorchModel, accuracy, samples
 
+import statistics
+
+
+epochs = 1
+
+#The values in the lists are the one we use as combinations
+
+#vth values
+#v_thh=[0.25,0.5,0.75,1.0,1.25,1.5,1.75,2.0,2.25]
+v_thh=[0.25]
+
+#T values
+# TT = [32,40,48,56,64,72,80]
+TT = [80]
+
+
+#We recommend using a GPU as the applying the attack to the SNN model takes a lot of time
+
+device = torch.device("cuda")
+mmodel = "super"
+lr = 0.001
+input_features = 32 * 32
 
 batchsize = 64
+
 
 transform = transforms.Compose(
     [transforms.Resize(32),
@@ -35,6 +55,7 @@ testset = torchvision.datasets.MNIST(root='./data', train=False,
                                        download=True, transform=transform)
 test_loader = torch.utils.data.DataLoader(testset, batch_size=batchsize,
                                          shuffle=False)
+
 
 class IFConstantCurrentEncoder(torch.nn.Module):
     def __init__(
@@ -56,6 +77,7 @@ class IFConstantCurrentEncoder(torch.nn.Module):
         lif_parameters = LIFParameters(tau_mem_inv=self.tau_mem_inv, v_th=self.v_th, v_reset=self.v_reset)
         return encode.constant_current_lif_encode(x, self.seq_length, p=lif_parameters, dt=self.dt)
 
+
 class ConvvNet4(torch.nn.Module):
     def __init__(
         self, device, num_channels=1, feature_size=32, method="super", dtype=torch.float
@@ -67,6 +89,7 @@ class ConvvNet4(torch.nn.Module):
         self.conv2 = torch.nn.Conv2d(6, 16, kernel_size=5,stride=1)
         self.conv3 = torch.nn.Conv2d(16, 120, kernel_size=5, stride=1)
         self.fc1 = torch.nn.Linear(120, 84)
+#         self.fc2 = torch.nn.Linear(84, 10)
 
         self.lif0 = LIFFeedForwardCell(p=LIFParameters(method=method, alpha=100.0))
         self.lif1 = LIFFeedForwardCell(p=LIFParameters(method=method, alpha=100.0))
@@ -81,6 +104,7 @@ class ConvvNet4(torch.nn.Module):
         seq_length = x.shape[0]
         batch_size = x.shape[1]
 
+        # specify the initial states
         s0 = None
         s1 = None
         s2 = None
@@ -101,6 +125,7 @@ class ConvvNet4(torch.nn.Module):
             z = 10 * self.conv3(z)
             z, s2 = self.lif2(z, s2)
             z = torch.nn.functional.relu(z)
+#           z = z.view(-1, 16*5*5)
             z = torch.flatten(z, 1)
             z = self.fc1(z)
             z, s3 = self.lif3(z, s3)
@@ -108,124 +133,172 @@ class ConvvNet4(torch.nn.Module):
             voltages[ts, :, :] = v
         return voltages
 
-        def train(model, device, train_loader, optimizer, epoch, writer=None):
-            model.train()
-            losses = []
 
-            batch_len = len(train_loader)
-            step = batch_len * epoch
+def train(model, device, train_loader, optimizer, epoch, writer=None):
+    model.train()
+    losses = []
 
-            for batch_idx, (data, target) in enumerate(train_loader):
-                data, target = data.to(device), target.to(device)
-                optimizer.zero_grad()
-                output = model(data)
-                loss = torch.nn.functional.nll_loss(output, target)
-                loss.backward()
+    batch_len = len(train_loader)
+    step = batch_len * epoch
 
-                optimizer.step()
-                step += 1
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = torch.nn.functional.nll_loss(output, target)
+        loss.backward()
 
-                if batch_idx % 100 == 0:
-                    print(
-                        "Train Epoch: {}/{} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
-                            epoch,
-                            epochs,
-                            batch_idx * len(data),
-                            len(train_loader.dataset),
-                            100.0 * batch_idx / len(train_loader),
-                            loss.item(),
-                        )
-                    )
+        optimizer.step()
+        step += 1
 
-                losses.append(loss.item())
-
-            mean_loss = np.mean(losses)
-            return losses, mean_loss
-
-
-        def test(model, device, test_loader, epoch, writer=None):
-            model.eval()
-            test_loss = 0
-            correct = 0
-            with torch.no_grad():
-                for data, target in test_loader:
-                    data, target = data.to(device), target.to(device)
-                    output = model(data)
-                    test_loss += torch.nn.functional.nll_loss(
-                        output, target, reduction="sum"
-                    ).item()
-                    pred = output.argmax(
-                        dim=1, keepdim=True
-                    )
-                    correct += pred.eq(target.view_as(pred)).sum().item()
-
-            test_loss /= len(test_loader.dataset)
-
-            accuracy = 100.0 * correct / len(test_loader.dataset)
+        if batch_idx % 100 == 0:
             print(
-                f"\nTest set {mmodel}: Average loss: {test_loss:.4f}, \
-                    Accuracy: {correct}/{len(test_loader.dataset)} ({accuracy:.0f}%)\n"
+                "Train Epoch: {}/{} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
+                    epoch,
+                    epochs,
+                    batch_idx * len(data),
+                    len(train_loader.dataset),
+                    100.0 * batch_idx / len(train_loader),
+                    loss.item(),
+                )
             )
 
-            return test_loss, accuracy
+        losses.append(loss.item())
 
-v_thh=[0.25,0.5,0.75,1.0,1.25,1.5,1.75,2.0,2.25]
+    mean_loss = np.mean(losses)
+    return losses, mean_loss
 
-TT = [32,40,48,56,64,72,80]
+
+def test(model, device, test_loader, epoch, writer=None):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += torch.nn.functional.nll_loss(
+                output, target, reduction="sum"
+            ).item()  # sum up batch loss
+            pred = output.argmax(
+                dim=1, keepdim=True
+            )  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    taccuracy = 100.0 * correct / len(test_loader.dataset)
+    print(
+        f"\nTest set {mmodel}: Average loss: {test_loss:.4f}, \
+            Accuracy: {correct}/{len(test_loader.dataset)} ({taccuracy:.0f}%)\n"
+    )
+
+    return test_loss, taccuracy
+
+
+class LIFConvNet(torch.nn.Module):
+    def __init__(
+        self,
+        input_features,
+        seq_length,
+        v_th,
+        model="super",
+        only_first_spike=False,
+    ):
+        super(LIFConvNet, self).__init__()
+        self.constant_current_encoder = IFConstantCurrentEncoder(seq_length=seq_length,v_th=v_th)
+        self.only_first_spike = only_first_spike
+        self.input_features = input_features
+        self.rsnn = ConvvNet4(method=model,device=device)
+        self.seq_length = seq_length
+
+    def forward(self, x):
+        batch_size = x.shape[0]
+        x = self.constant_current_encoder(
+            x.view(-1, self.input_features) * 1
+        )
+        if self.only_first_spike:
+            # delete all spikes except for first
+            zeros = torch.zeros_like(x.cpu()).detach().numpy()
+            idxs = x.cpu().nonzero().detach().numpy()
+            spike_counter = np.zeros((batchsize, 32 * 32))
+            for t, batch, nrn in idxs:
+                if spike_counter[batch, nrn] == 0:
+                    zeros[t, batch, nrn] = 1
+                    spike_counter[batch, nrn] += 1
+            x = torch.from_numpy(zeros).to(x.device)
+
+        x = x.reshape(self.seq_length, batch_size, 1, 32, 32)
+        voltages = self.rsnn(x)
+        m, _ = torch.max(voltages, 0)
+        log_p_y = torch.nn.functional.log_softmax(m, dim=1)
+        return log_p_y
+
+
+#This function does the same work as foolbox, it allows us to compare the values of foolbox
+#It's only used to check if there is no issues with the model given to foolbox
+
+def benchmark():
+    listeimg = []
+    listeadv = []
+    for i in range(len(images)):
+      imaj = images[i]
+      img = imaj
+      listeimg.append(img)
+      tempadvlist = []
+      for e in range(len(epsilons)):
+        iadvs = advs[e][i]
+        adv = iadvs
+        tempadvlist.append(adv)
+      listeadv.append(tempadvlist)
+
+    results = []
+
+    model.eval()
+    for e in range(len(epsilons)):
+      tempepsilons = []
+      for i in range(len(listeadv)):
+        imgadvtest = listeadv[i][e]
+        lb = labels[i].item()
+        with torch.no_grad():
+          output = model(imgadvtest.unsqueeze(0))
+        pred = output.data.max(1, keepdim=True)[1][0].item()
+        prob = torch.nn.functional.softmax(output, dim=1)
+        top_p, top_class = prob.topk(1, dim = 1)
+        if(str(lb)==str(pred)):
+          tempepsilons.append("FALSE")
+        else:
+          tempepsilons.append("TRUE")
+      results.append(tempepsilons)
+
+    for i in range(len(results)):
+        v = 0
+        for e in range(len(results[0])):
+            if results[i][e] == "FALSE":
+                v = v+1
+        print("eps: "+ str(epsilons[i]), end=' | ')
+        print("pred en %: " + str(float(v/len(results[0]))*100))
+        print("")
+
 
 for i in range(len(TT)):
     for j in range(len(v_thh)):
 
         print(TT[i],v_thh[j])
+        print(" ")
 
-        class LIFConvNet(torch.nn.Module):
-            def __init__(
-                self,
-                input_features,
-                seq_length,
-                model="super",
-                only_first_spike=False,
-            ):
-                super(LIFConvNet, self).__init__()
-                self.constant_current_encoder = IFConstantCurrentEncoder(seq_length=seq_length,v_th=v_thh[j])
-                self.only_first_spike = only_first_spike
-                self.input_features = input_features
-                self.rsnn = ConvvNet4(method=model,device=device)
-                self.seq_length = seq_length
-
-            def forward(self, x):
-                batch_size = x.shape[0]
-                x = self.constant_current_encoder(
-                    x.view(-1, self.input_features) * 1
-                )
-                if self.only_first_spike:
-                    zeros = torch.zeros_like(x.cpu()).detach().numpy()
-                    idxs = x.cpu().nonzero().detach().numpy()
-                    spike_counter = np.zeros((batchsize, 32 * 32))
-                    for t, batch, nrn in idxs:
-                        if spike_counter[batch, nrn] == 0:
-                            zeros[t, batch, nrn] = 1
-                            spike_counter[batch, nrn] += 1
-                    x = torch.from_numpy(zeros).to(x.device)
-
-                x = x.reshape(self.seq_length, batch_size, 1, 32, 32)
-                voltages = self.rsnn(x)
-                m, _ = torch.max(voltages, 0)
-                log_p_y = torch.nn.functional.log_softmax(m, dim=1)
-                return log_p_y
-
-        device = torch.device("cuda")
-        mmodel = "super"
-        lr = 0.001
         T = TT[i]
-        input_features = 32 * 32
+        v = v_thh[j]
 
         model = LIFConvNet(
             input_features=input_features,
             seq_length=T,
+            v_th=v,
+#             model=mmodel,
+#             only_first_spike=False,
         ).to(device)
+
         optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-        epochs = 1
 
         training_losses = []
         mean_losses = []
@@ -234,12 +307,12 @@ for i in range(len(TT)):
 
         for epoch in range(epochs):
                 training_loss, mean_loss = train(model, device, train_loader, optimizer, epoch)
-                test_loss, accuracy = test(model, device, test_loader, epoch)
+                test_loss, taccuracy = test(model, device, test_loader, epoch)
 
                 training_losses += training_loss
                 mean_losses.append(mean_loss)
                 test_losses.append(test_loss)
-                accuracies.append(accuracy)
+                accuracies.append(taccuracy)
 
                 max_accuracy = np.max(np.array(accuracies))
 
@@ -252,9 +325,12 @@ for i in range(len(TT)):
         images = images.to(device)
         labels = labels.to(device)
 
+        print(model)
+
         fmodel = fb.PyTorchModel(model, bounds=(0, 1))
 
-        print("accuracy")
+        print(" ")
+        print("accuracy", end=' | ')
         print(accuracy(fmodel, images, labels))
         print("")
 
@@ -284,19 +360,27 @@ for i in range(len(TT)):
             2.0,
         ]
 
-        attack_success = np.zeros((len(attacks), len(epsilons), len(images)), dtype=np.bool)
-        for i, attack in enumerate(attacks):
-            _, _, success = attack(fmodel, images, labels, epsilons=epsilons)
-            assert success.shape == (len(epsilons), len(images))
-            success_ = success.cpu().numpy()
-            assert success_.dtype == np.bool
-            attack_success[i] = success_
-            print(attack)
-            print("  ", 1.0 - success_.mean(axis=-1).round(2))
+        print("epsilons")
+        print(epsilons)
+        print("")
 
-        robust_accuracy = 1.0 - attack_success.max(axis=0).mean(axis=-1)
-        print("")
-        print("-" * 79)
-        print("")
-        print("worst case (best attack per-sample)")
-        print("  ", robust_accuracy.round(2))
+        lbyat = []
+        fulatac = []
+        for i, attack in enumerate(attacks):
+            _, advs, success = attack(fmodel, images, labels, epsilons=epsilons)
+            benchmark()
+            atacc = []
+            robust_accuracy = 1 - success.float().mean(axis=-1)
+            for eps, acc in zip(epsilons, robust_accuracy):
+                print(attack, eps, acc.item())
+                atacc.append(acc.item())
+            print(" ")
+            fulatac.append(atacc)
+            lfull = []
+            for e in range(len(epsilons)):
+                l = []
+                for i in range(len(images)):
+                    perturbation = advs[e][i][0].cpu().numpy() - images[i][0].cpu().numpy()
+                    l.append(float(format(np.linalg.norm(perturbation.flatten()))))
+                lfull.append(statistics.mean(l))
+            lbyat.append(lfull)
